76c7e70c01d85dbc0430f6d8d74637b15c694471
Skam <_skam_@mail.ru>
2022-10-17 18:21:53 +0500

    add HomeWork_1_1

--- HomeWork_1_1/Program.cs
+++ HomeWork_1_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2; b = 10 -> max = 10
+a = -9; b = -3 -> max = -3 */
+
+Console.WriteLine("Введите первое число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+ 
+if (numA > numB)
+{
+    max = numA;
+}
+else
+{
+    max = numB;
+}
+Console.WriteLine("Максимальное число = " + max);
