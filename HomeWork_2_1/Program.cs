eb0ae2559c6168d360cea6689a29c61e220a64ad
Skam <_skam_@mail.ru>
2022-10-19 16:32:34 +0500

    add homework_2_1

---
+++ HomeWork_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. (Использовать только математические операции, нельзя использовать число как строку по типу number[1])
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetIntConsole()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int numA = GetIntConsole();
+
+while (true) 
+{
+    if (numA < 99 || numA > 1000)
+    {
+        Console.WriteLine("Вы ввели не трехзначное число, повторите ввод или введите цифру 0 для выхода");
+        numA = GetIntConsole();
+        if (numA == 0)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else
+    {
+        int rezult = (numA % 100) / 10;
+        Console.WriteLine(rezult);
+        break;
+    }
+}
