9e7132567260581360d558281a223b49d0c4b9f5
Skam <_skam_@mail.ru>
2022-10-20 15:42:02 +0500

    add task_!

---
+++ additional_task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 1. В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение */
+
+string secretMessage = "Hello!";
+string passwrd = "Hi";
+int count = 3;
+
+while (count != 0)
+{
+    Console.Write("Введите пароль:");
+    string inputUser = Console.ReadLine();
+    if (inputUser == passwrd)
+    {
+        Console.Write(secretMessage);
+        break;
+    }
+    else
+    {
+        count--;
+    }
+}
