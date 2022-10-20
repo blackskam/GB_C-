25a46ecb65ec7e13e0cf1357c1c205a28b8f461e
Skam <_skam_@mail.ru>
2022-10-20 16:12:26 +0500

    add task_2

---
+++ additional_task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 1. 2. Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается */
+
+while (true)
+{
+    string inputUser = Console.ReadLine();
+    if (inputUser == "exit")
+    {
+        break;
+    }
+}
