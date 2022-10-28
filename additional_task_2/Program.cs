38228beeef6df03b1bf0be3f34fc64fe6cf7b4e7
Skam <_skam_@mail.ru>
2022-10-28 11:28:12 +0500

    add corrections based on comments

--- additional_task_2/Program.cs
+++ additional_task_2/Program.cs
@@ -7,13 +7,22 @@ SetPassword – Установить пароль
 Exit – выход
 WriteName – вывести имя после ввода пароля */
 
+/* 1. WriteName можно ввести пока еще не установлены имя и пароль, что немного нелогично 
+2. Получилось прям очень простенько, давай еще усложним команду ввода пароля. Если пароль уже установлен, то сначала нужно ввести старый пароль */
+
+string ReadString(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
 string passwordUser = " ";
 string nameUser = " ";
 bool exitTrue = true;
 
 while (exitTrue)
 {
-    string inputUser = Console.ReadLine().ToLower();
+    string inputUser = ReadString("Введите команду: ").ToLower();
     switch (inputUser)
     {
         case "exit":
@@ -25,28 +34,88 @@ while (exitTrue)
             {
                 Console.WriteLine("exit -> выход");
                 Console.WriteLine("help -> вывести список комманд");
-                Console.WriteLine("setname -> установить имя");
-                Console.WriteLine("setpassword -> установить пароль");
+                Console.WriteLine("setname -> установить имя и пароль");
+                Console.WriteLine("changepassword -> сменить пароль");
                 Console.WriteLine("writename -> вывести имя после ввода пароля");
+                Console.WriteLine("changename -> сменить имя после ввода пароля");
                 break;
             }
         case "setname":
             {
-                nameUser = Console.ReadLine();
+                nameUser = ReadString("Введите имя: ");
+                passwordUser = ReadString("Введите пароль: ");
                 break;
             }
-        case "setpassword":
+        case "changepassword":
             {
-                passwordUser = Console.ReadLine();
+                if (nameUser != " ")
+                {
+                    string chekNameUser = ReadString("Введите имя: ");
+                    if (chekNameUser == nameUser)
+                    {
+                        string chekPasswordUser = ReadString("Введите пароль: ");
+                        if (chekPasswordUser == passwordUser)
+                        {
+                            passwordUser = ReadString("Введите новый пароль: ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Пароль неверный!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Такого имени не существует!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Имя не установлено!");
+                }
                 break;
             }
         case "writename":
             {
-                Console.WriteLine("Введите пароль:");
-                string inputPasswordUser = Console.ReadLine();
-                if (inputPasswordUser == passwordUser)
+                if (nameUser != " ")
                 {
-                    Console.WriteLine(nameUser);
+                    string inputPasswordUser = ReadString("Введите пароль: ");
+                    if (inputPasswordUser == passwordUser)
+                    {
+                        Console.WriteLine(nameUser);
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Имя не установлено!");
+                }
+                break;
+            }
+        case "changename":
+            {
+                if (nameUser != " ")
+                {
+                    string chekNameUser = ReadString("Введите имя: ");
+                    if (chekNameUser == nameUser)
+                    {
+                        string chekPasswordUser = ReadString("Введите пароль: ");
+                        if (chekPasswordUser == passwordUser)
+                        {
+                            nameUser = ReadString("Введите новое имя: ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Пароль неверный!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Такого имени не существует!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Имя не установлено!");
                 }
                 break;
             }
