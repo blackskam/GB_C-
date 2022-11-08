b9d7805f0f9f866bb9301d15d26dd3889affbc7c
Skam <_skam_@mail.ru>
2022-11-08 14:43:45 +0500

    add homework_4_2

---
+++ HomeWork_4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadInt("Введите число: ");
+
+int count = 0;
+while (numberA > 9)
+{
+    count += numberA % 10;
+    numberA /= 10;
+}
+count += numberA;
+Console.WriteLine("Сумма цифр равна " + count);
