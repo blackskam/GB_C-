541927bd7fedc66e6d153f9f854ce8d4bac5a855
Skam <_skam_@mail.ru>
2022-11-08 15:00:55 +0500

    add homework_4_3

---
+++ HomeWork_4_3/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadInt("Введите размер массива: ");
+
+int[] array = new int[numberA];
+ 
+for (int i = 0; i < numberA; i++)
+{
+    Console.WriteLine("Введите {0} элемент массива: ", i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Ваш введенный массив: [");
+for (int i = 0; i < numberA; i++)
+{
+    Console.Write(array[i]);
+    if (i < (numberA - 1))
+    {
+        Console.Write(", ");
+    }
+}
+Console.Write("]");
