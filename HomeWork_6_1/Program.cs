9557f39ca7368817a82f0e323458f52369062734
Skam <_skam_@mail.ru>
2022-11-21 16:23:57 +0500

    add home_work_6_1

---
+++ HomeWork_6_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 4 */
+
+
+string numbers = ReadInt("Введите числа через запятую:");
+string[] strlist = numbers.Split(new char[] {','});
+Console.WriteLine(CountPositiv(strlist));
+
+int CountPositiv(string[] strlst)
+{
+    int k = 0;
+    for (int i = 0; i < strlist.Length; i++)
+    {
+        if (Convert.ToInt32(strlist[i]) > 0)
+        k++;
+    }
+    return k;
+}
+string ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
