7a85d505bf2a5cb97bc8e9c1c547b13725784785
Skam <_skam_@mail.ru>
2022-12-01 19:07:49 +0500

    add homework_9_2

---
+++ HomeWork_9_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 66: Задайте значения M и N.
+ Напишите программу, которая найдёт сумму натуральных элементов в промежутке
+  от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  */
+
+
+
+int numM = 4;
+int numN = 8;
+GetNumberSum(numM, numN, 0);
+
+
+void GetNumberSum(int numM, int numN, int sum)
+{
+    if (numM > numN)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum += numM++;
+    GetNumberSum(numM, numN, sum);
+}
+
